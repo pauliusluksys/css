7b1bf3739a1b110009563893012f22be367135dc
pauliusluksys <58655050+pauliusluksys@users.noreply.github.com>
2020-11-16 16:40:10 +0200

    Add files via upload

---
+++ www.wordstream.com/index.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta content="width=device-width, initial-scale=1" name="viewport" />
+	<title>Word Stream</title>
+	<link rel="stylesheet" href="index.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<script
+  	src="https://code.jquery.com/jquery-3.5.1.min.js"
+  	integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  	crossorigin="anonymous"></script>
+</head>
+<header>
+	<div class="container-fluid">
+		<div class="row bg-primary text-white py-3">
+			<div class="col-md-1"></div>
+			<div class="col-md-10 px-0">
+				<div class="container-fluid">
+					<div class="row topper">
+						<div class="top-left ">
+							<div class="first-telephone-top">
+								<div class="telephone-icon "></div>
+								<div class="telephone-number d-flex align-items-center">+861111111</div>
+							</div>
+							<div class="second-telephone-top">
+								<div class="telephone-icon"></div>
+								<div class="telephone-number d-flex align-items-center">874444444</div>
+							</div>
+								
+							</div>
+						<div class="display-right">
+							<div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
+								<button type="button" class="btn btn-secondary">Grader</button>
+ 							    <button type="button" class="btn btn-secondary">Login</button>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div class="col-md-1"></div>
+		</div>	
+	</div>
+
+	<div class="container-fluid">
+		<div class="row main-menu-row">
+			
+				
+					<div class="main-header">
+
+						<div class="center-children">
+							<div class="logo-container-top bg-dark">
+								<img src = "wave1.jpg" class= "img-thumbnail logo-picture"/>
+							</div>
+						</div>
+						<div class="header-mid-spacing"></div>
+						<div class="main-menu">
+							<nav class="navbar navbar-expand-lg navbar-light">
+							  
+							  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+							    <span class="navbar-toggler-icon"></span>
+							  </button>
+							  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+							    <div class="navbar-nav">
+							      <a class="nav-item nav-link active" href="#">Our Solutions<span class="sr-only">(current)</span></a>
+							      <a class="nav-item nav-link active" href="#">How We help<span class="sr-only">(current)</span></a>
+							      <a class="nav-item nav-link active" href="#">Home<span class="sr-only">(current)</span></a>
+							      <a class="nav-item nav-link active" href="#">Blog<span class="sr-only">(current)</span></a>
+							    </div>
+							  </div>
+							</nav>
+
+						</div>
+					</div>
+				
+			</div>
+			
+		</div>
+	</div>
+
+</header>
+<body>
+<main>
+	<div class="container-fluid">
+	<div class="row bg-primary text-white py-3">
+		<div class="col-md-1 "></div>
+		<div class="col-md-6 ">
+			<H1>The WordStream Blog</H1>
+			<h3>Tips & tricks to help you get the most out of your online advertising.</h3>
+		</div>
+		<div class="col-md-5"></div>
+	</div>		
+	<div class="row">
+		<div class="col-md-1 "></div>
+		<div class="col-md-11 ">Home:Blog</div>
+	</div>		
+	<div class="row">
+		<div class="col-md-1 align-right-side d-none d-lg-block">
+			<div class="social-links view-element-container-sticky">
+				<div class="twitter-container"></div>
+				<div class="facebook-container"></div>
+				<div class="in-container"></div>
+			</div>
+		</div>
+		<div class="col-md-7 border-right">
+			<div id="article-title">Lorem Ipsum</div>
+			<div id="article-author">
+				<div id="article-author-img"></div>
+				<div id="article-author-name">aaaa bbbbb</div>
+			</div>
+			<div id="article-last-updated">Last updated: August 15, 2020</div>
+			<div id="article-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec purus eros, pulvinar in nisi tempus, ullamcorper consequat enim. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur eget eleifend ipsum, in pretium diam. Integer malesuada lectus erat, a tempus dolor euismod eget. Vestibulum euismod, lectus quis vehicula varius, odio nulla varius nisi, vitae sollicitudin mauris justo eu orci. Cras vestibulum arcu quis ex volutpat dictum. Sed quis mauris fermentum, blandit enim at, tempor felis. Proin in nisi nec mauris vestibulum commodo vel non lacus.
+
+Praesent ut nibh dignissim, accumsan erat vitae, suscipit justo. Pellentesque scelerisque sit amet sem sed tempor. Sed maximus bibendum augue eu porta. Cras laoreet arcu ex. Suspendisse imperdiet tempus est, non dictum arcu dictum pretium. Nunc sed iaculis neque. Duis vehicula odio nec lobortis gravida. Aliquam at sem a dolor vehicula vulputate. Curabitur purus diam, vulputate ut arcu at, ornare faucibus mi. Proin fermentum purus sem, efficitur malesuada augue imperdiet quis. Curabitur sit amet arcu placerat est faucibus cursus. Integer sollicitudin non nisl ac ullamcorper. Phasellus eget vestibulum turpis, ut mattis odio. Phasellus eros libero, ultrices vel tortor vitae, sagittis placerat ex. Proin lobortis tellus risus, eu dapibus purus congue quis.
+
+Proin tempor congue posuere. Morbi posuere mollis elementum. Proin tincidunt elit et nulla laoreet, sit amet fringilla turpis sodales. Curabitur et enim eget mauris consequat sodales a eu nibh. Nunc bibendum quis nisi ac mollis. In dapibus urna eu nunc pellentesque vestibulum. Pellentesque vitae rutrum metus, vitae maximus augue.
+
+In hac habitasse platea dictumst. Vivamus nec metus egestas, dapibus purus nec, hendrerit lacus. In vehicula iaculis efficitur. Morbi viverra ligula id neque tempus, non viverra enim lobortis. Aenean a felis tincidunt, tristique mi id, lacinia massa. Cras a leo sed turpis tristique tincidunt. Curabitur eget efficitur est. Ut tristique finibus nisl ac condimentum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum rhoncus felis sem, ac dignissim lacus varius ultrices. Vestibulum arcu leo, rutrum eget massa nec, porttitor lobortis nulla. Duis sagittis maximus fringilla. Proin laoreet enim sit amet ullamcorper rhoncus. Etiam malesuada blandit consequat.
+
+Donec pretium sapien id tellus lacinia, a lacinia lorem malesuada. Cras ex lacus, lobortis non blandit elementum, egestas at tellus. Donec cursus nibh vitae lectus pretium, porttitor facilisis felis sollicitudin. Suspendisse potenti. Morbi nisl mi, congue ac augue vitae, finibus pharetra lorem. Aenean sed odio congue, lobortis mauris id, tristique lacus. Aliquam erat volutpat. Morbi eget justo velit. Pellentesque ut dui in libero pretium faucibus. Quisque blandit tortor et semper fermentum. Nulla pharetra magna eu mi vehicula hendrerit. Mauris placerat et tortor eu laoreet. Nullam non neque lacinia diam vulputate pulvinar nec quis felis. Integer luctus lacinia mauris. Proin eleifend quam nec ligula laoreet, et porta lacus dapibus.</div>
+		</div>
+		<div class="col-md-4">
+			<div class="right-side-content">
+				<div class="ct">
+					<div class="header-ct">
+						<h4>Capture & Convert More Leads Today</h4>
+					</div>
+					<div class="main-ct">
+						<div class="img-ct"></div>
+						<div class="text-ct">Use the Conversion Toolkit to maximize your advertising budget.</div>
+						<button class="button-ct">Try for free!</button>
+					</div>
+				</div>
+				<div class="tips-tricks">
+					<div class="text-container"><H3>Get Some New Tricks!</H3></div>
+					<button class="btn btn-primary signup-tt" >SIGN ME UP</button>
+				</div>
+
+				<div class="related-content">
+					<div class="header-rc">Related content</div>
+						<div class="item-rc">
+							
+								<div class="img-rc">
+								<img src="smile-quotes.jpg" class ="img-thumbnail">
+								</div>
+							
+							<div class="text-rc"><h6> ullamcorper. Phasellus eget vestibulum turpis, ut mattis odio. Phasellus eros libero, </h6></div>
+						</div>
+						<div class="item-rc">
+							
+								<div class="img-rc">
+								<img src="smile-quotes.jpg" class ="img-thumbnail">
+								</div>
+							
+							<div class="text-rc"><h6> ullamcorper. Phasellus eget vestibulum turpis, ut mattis odio. Phasellus eros libero, </h6></div>
+						</div>
+						<div class="item-rc">
+							
+								<div class="img-rc">
+								<img src="person-1.jpeg" class ="img-thumbnail">
+								</div>
+							
+							<div class="text-rc"><h6> ullamcorper. Phasellus eget vestibulum turpis, ut mattis odio. Phasellus eros libero, </h6></div>
+						</div>
+						<div class="item-rc">
+							
+								<div class="img-rc">
+								<img src="smile-quotes.jpg" class ="img-thumbnail">
+								</div>
+							
+							<div class="text-rc"><h6> ullamcorper. Phasellus eget vestibulum turpis, ut mattis odio. Phasellus eros libero, </h6></div>
+						</div>
+					
+				</div>
+
+				<div class="cool-tools-fixed view-element-container-sticky">
+					<div class="ct ">
+					<div class="header-ct">
+						<h4>Capture & Convert More Leads Today</h4>
+					</div>
+					<div class="main-ct">
+						<div class="img-ct"></div>
+						<div class="text-ct">Use the Conversion Toolkit to maximize your advertising budget.</div>
+						<button class="button-ct">Try for free!</button>
+					</div>
+				</div>
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+</div>
+
+</main>
+<footer>
+	<div class="container-fluid footer-container">
+		<div class="row no-gutters">
+			<div class="col-md-1"></div>
+			<div class="col-md-10">
+				<div class="footer-upper">
+				<div class="left-footer-upper">
+					<div class="logo-footer ">
+						<img src="wave1.jpg" class="img-fluid display-center"/>
+					</div>
+				</div>
+				<div class="partner-footer">
+					<div class="partner"><img src="ot-contrib-google.png"class = "img-fluid display-center"/></div>
+					<div class="partner"><img src="ot-contrib-google.png" class = "img-fluid display-center"/></div>
+					<div class="partner"><img src="ot-contrib-google.png" class = "img-fluid display-center"/></div>
+				</div>
+				</div>
+				<div class="footer-middle">
+					<div class="container-fluid p-0 mt-4">
+						<div class="row no-gutters">
+							<div class="col-md-3">
+								<button class="make-call">Make A Call</button>
+								<div class="text-company-learn d-flex">
+									<div class="company"><h2>Company</h2>
+										<h5>r nec quis</h5>
+										<h5>r nec quis</h5>
+										<h5>r nec</h5>
+										<h5>r ne</h5></div>
+									<div class="learn">
+										<h2>Learn</h2>
+										<h5>r nec quis</h5>
+										<h5>r nec quis</h5>
+										<h5>r nec</h5>
+										<h5>r ne</h5></div>
+									</div>
+								</div>
+							
+							<div class="col-md-3"></div>
+							<div class="col-md-6">
+								<div class="icons-go-to">
+									<div class="icon rounded-circle"><a href="#"><img src=""/></div>
+									<div class="icon rounded-circle"><a href="#"><img src=""/></div>
+								</div>
+								<div class="footer-main-links">
+									<ul class="list-fix">
+										<li><a href="#"> rhoncus felis sem, ac dignissim lacus varius ultrices. Vestibulum arcu leo, rutrum eget massa nec, porttito</a</li>
+										<li><a href="#"> rhoncus felis sem, ac dignissim lacus varius ultrices. Vestibulum arcu leo, rutrum eget massa nec, porttito</a</li>
+										<li><a href="#"> rhoncus felis sem, ac dignissim lacus varius ultrices. Vestibulum arcu leo, rutrum eget massa nec, porttito</a</li>
+										<li><a href="#"> rhoncus felis sem, ac dignissim lacus varius ultrices. Vestibulum arcu leo, rutrum eget massa nec, porttito</a</li>
+											<li><a href="#"> rhoncus felis sem, ac dignissim lacus varius ultrices. Vestibulum arcu leo, rutrum eget massa nec, porttito</a</li>
+												<li><a href="#"> rhoncus felis sem, ac dignissim lacus varius ultrices. Vestibulum arcu leo, rutrum eget massa nec, porttito</a</li>
+													<li><a href="#"> rhoncus felis sem, ac dignissim lacus varius ultrices. Vestibulum arcu leo, rutrum eget massa nec, porttito</a</li>
+														<li><a href="#"> rhoncus felis sem, ac dignissim lacus varius ultrices. Vestibulum arcu leo, rutrum eget massa nec, porttito</a</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="md-col1"></div>
+		</div>
+	</div>
+
+</footer>
+
+</body>
+</html>
